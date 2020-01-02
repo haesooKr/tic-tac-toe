683072de24c2b5f1bdbe98e8b744473fee89d0b3
haesooKr <gotn676@gmail.com>
2020-01-02 16:53:10 -0500

    Added CalculateWinner func

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -8,6 +8,7 @@ export default class Board extends Component {
     }
     handleClick(i){
         const squares = [...this.state.squares];
+        if(calculateWinner(squares)) return null;
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares,
@@ -22,7 +23,13 @@ export default class Board extends Component {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
   
       return (
         <div>
@@ -46,3 +53,25 @@ export default class Board extends Component {
       );
     }
   }
+
+const calculateWinner = (squares) => {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(let line of lines){
+        const [a,b,c] = line;
+        if( squares[a] && 
+            squares[a] === squares[b] && 
+            squares[a] === squares[c]) {
+            return squares[a];
+        }
+        return null;
+    }
+}
