e1e42e43d9178b24b63aa52078c7a152c51275ee
haesooKr <gotn676@gmail.com>
2020-01-02 17:48:51 -0500

    Added Game History

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -6,10 +6,12 @@ export default class Game extends Component {
         history: [{
             squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true
     }
     handleClick(i){
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1)
+        console.log(history);
         const current = history[history.length - 1];
         const squares = [...current.squares];
         
@@ -19,13 +21,32 @@ export default class Game extends Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
         });
     }
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        })
+    }
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber]
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
         let status;
         if(winner){
             status = `Winner is ${winner}`
@@ -42,7 +63,7 @@ export default class Game extends Component {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
