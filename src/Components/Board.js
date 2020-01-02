369372b90cae016b5600b2268532816c19117afb
haesooKr <gotn676@gmail.com>
2020-01-02 16:37:01 -0500

    Modified handleClick func

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,12 +3,16 @@ import Square from './Square';
 
 export default class Board extends Component {
     state = {
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        xIsNext: true
     }
     handleClick(i){
         const squares = [...this.state.squares];
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
     renderSquare(i) {
       return <Square 
@@ -18,7 +22,7 @@ export default class Board extends Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 
-export default class Square extends Component {
-  render() {
+export default function Square(props) {
     return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-        >
-        {this.props.value}
-      </button>
-    );
-  }
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
